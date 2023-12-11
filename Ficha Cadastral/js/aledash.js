7c4fb9460b7c2c11ff382d6e1ecedf8a22f250d0
ALEXANDRE MAXIMIANO DE FREITAS <alexandremaximiano72@gmail.com>
2023-12-11 07:33:08 -0300

    Add files via upload

---
+++ Ficha Cadastral/js/aledash.js
@@ -0,0 +1,32 @@
+gapi.load('client', function() {
+            gapi.client.init({
+                apiKey: 'AIzaSyAxGbdWe639fXoxSAnogiMC_sJBFn5uXsk',
+                discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+            }).then(function() {
+                document.getElementById("buscarButton").addEventListener("click", buscarInformacoes);
+            });
+        });
+
+        function buscarInformacoes() {
+            const chave = document.getElementById("chaveInput").value;
+            
+            gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: '1FGQFL04OoclymsoGV5okUp_mw_Pkjq9uHJa0Yr9l3ps',
+                range: 'VEICULO!A:DS',
+            }).then(response => {
+                const values = response.result.values;
+                if (values && values.length > 0) {
+                    for (let i = 0; i < values.length; i++) {
+                        if (values[i][0] === chave) {
+                            document.getElementById("nomecompletomotorista").innerText = values[i][0];
+                            return; // Encerra o loop após encontrar a chave
+                        }
+                    }
+                    document.getElementById("status").innerText = "Planilha não encontrada.";
+                } else {
+                    document.getElementById("status").innerText = "Nenhum dado encontrado.";
+                }
+            }, response => {
+                console.error('Erro ao buscar informações:', response.result.error.message);
+            });
+        }
