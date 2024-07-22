a196a243abbb59ff156f3de9bb6eb00d4a3e944c
ALEXANDRE MAXIMIANO DE FREITAS <alexandremaximiano72@gmail.com>
2024-07-22 20:59:31 -0300

    Update app-profile-out.js

--- js/app-profile-out.js
+++ js/app-profile-out.js
@@ -1,3 +1,19 @@
+// auth.js
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user && user.displayName) {
+        const userName1 = document.getElementById('user-name1');
+        const userName2 = document.getElementById('user-name2');
+        if (userName1) userName1.innerText = user.displayName;
+        if (userName2) userName2.innerText = "Olá, " + user.displayName;
+
+        loadUserData(user.uid);
+        loadUserPosts(user.uid, 'posts-container');
+        loadNotifications(user.uid, 'notification-container');
+    } else {
+        window.location.href = "page-login.html";
+    }
+});
+
 // Função de logout
 document.getElementById('logout').addEventListener('click', function() {
     firebase.auth().signOut().then(() => {
