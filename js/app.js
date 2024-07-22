f203c93a0b88c12eb901a5ecc21faa8ae6b5d1b2
ALEXANDRE MAXIMIANO DE FREITAS <alexandremaximiano72@gmail.com>
2024-07-22 19:27:38 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -161,3 +161,12 @@ document.getElementById('change-password-form').addEventListener('submit', funct
         console.error('Erro ao reautenticar o usuário:', error);
     });
 });
+
+// Função de logout
+document.getElementById('logout').addEventListener('click', function() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = "page-login.html";
+    }).catch((error) => {
+        console.error('Erro ao fazer logout:', error);
+    });
+});
