b108b2bc7ee8db1f7c706a5dc7b0e77c6c89fe22
ALEXANDRE MAXIMIANO DE FREITAS <alexandremaximiano72@gmail.com>
2024-07-23 06:49:06 -0300

    Add files via upload

---
+++ js/b-auth.js
@@ -0,0 +1,25 @@
+import { loadUserData } from './userData.js';
+import { loadUserPosts } from './userPosts.js';
+import { loadNotifications } from './notifications.js';
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user && user.displayName) {
+        const userName1 = document.getElementById('user-name1');
+        if (userName1) userName1.innerText = user.displayName;
+
+        loadUserData(user.uid);
+        loadUserPosts(user.uid, 'posts-container');
+        loadNotifications(user.uid, 'notification-container');
+    } else {
+        window.location.href = "page-login.html";
+    }
+});
+
+// Função de logout
+document.getElementById('logout').addEventListener('click', function() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = "page-login.html";
+    }).catch((error) => {
+        console.error('Erro ao fazer logout:', error);
+    });
+});
