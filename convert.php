e97d6e378e7d0b4605b564c16b9fc3cda0127fd6
ALEXANDRE MAXIMIANO DE FREITAS <alexandremaximiano72@gmail.com>
2024-07-30 06:06:04 -0300

    ok

--- convert.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $pdfFile = $_FILES['pdfFile']['tmp_name'];
-  $pdf = new Imagick();
-  $pdf->readImage($pdfFile);
-  $pdf->setImageFormat('jpg');
-  header('Content-Type: image/jpeg');
-  echo $pdf;
-?>
