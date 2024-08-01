0a546f9d53528255f8c821ea46b1f5e8cc3d83a1
ALEXANDRE MAXIMIANO DE FREITAS <alexandremaximiano72@gmail.com>
2024-08-01 06:32:21 -0300

    Add files via upload

---
+++ inactivity-logout.js
@@ -0,0 +1,41 @@
+// inactivity-logout.js
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+
+const logoutUser = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        window.location.href = 'page-login.html';
+    }).catch((error) => {
+        console.error('Erro ao realizar logout:', error);
+    });
+};
+
+let inactivityTimeout;
+
+const resetInactivityTimeout = () => {
+    clearTimeout(inactivityTimeout);
+    inactivityTimeout = setTimeout(logoutUser, 1 * 1 * 1000); // 1 minuto para realizar os testes || para 1 hora 3600 * 1000
+};
+
+const initializeInactivityHandler = () => {
+    resetInactivityTimeout(); // Inicializa o timeout
+    window.onload = resetInactivityTimeout;
+    document.onmousemove = resetInactivityTimeout;
+    document.onkeydown = resetInactivityTimeout;
+    document.onscroll = resetInactivityTimeout;
+    document.onclick = resetInactivityTimeout;
+    window.onbeforeunload = () => {
+        // Marca a sessão como inativa no localStorage quando a aba é fechada
+        localStorage.setItem('sessionActive', 'false');
+    };
+};
+
+// Checa a sessão ativa quando a página é carregada
+if (localStorage.getItem('sessionActive') === 'false') {
+    logoutUser();
+}
+
+// Marca a sessão como ativa quando a página é carregada
+localStorage.setItem('sessionActive', 'true');
+
+export { initializeInactivityHandler, logoutUser };
